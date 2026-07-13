c50250ec0a177750e90e76613155bb2567f3c9bf
Mantej05 <mantejhanda873@gmail.com>
2026-07-13 11:06:15 +0530

    Remove debug print statements

--- project_urls.py
+++ project_urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-print("=== LOADING PROJECT URLS ===")
-
 urlpatterns = [
     path('admin/',   admin.site.urls),
     path('',         include('cabs.urls')),
